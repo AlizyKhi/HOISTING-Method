9eb05148136b34758497876ad7ed09529c37c6da
AlizyKhi <162714208+AlizyKhi@users.noreply.github.com>
2024-05-15 23:39:12 +0500

    Add files via upload
    How To Practice Hoisting Method Easiest Way

---
+++ main.ts
@@ -0,0 +1,35 @@
+console.log(fullName)
+let fullName="alizyKhan";
+
+console.log(khanAlizy)
+var khanAlizy="fullName";
+
+console.log(kuchHeavyWala)
+const kuchHeavyWala="carWar";
+
+let halfName="alizyKhan";
+console.log(halfName)
+
+var khanAlizy="fullName";
+console.log(khanAlizy)
+
+const kuchWala="carWar";
+console.log(kuchWala)
+
+let surmaWala="kajol";
+console.log(surmaWala)
+
+var fabric="cottonCandy";
+console.log(fabric)
+
+const requirementSize="Double";
+console.log(requirementSize)
+
+console.log(fontPaper)
+const fontPaper="Size";
+
+console.log(cottonCandy)
+var cottonCandy="fabric";
+
+console.log(dataTypes)
+let dataTypes="unDefine";
